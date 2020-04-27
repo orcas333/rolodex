12bb8800776339785480f0680e90053a885268ef
Jenny Hu <huj@mit.edu>
2020-04-27 13:46:08 -0700

    adding files for github pages

---
+++ build/precache-manifest.4e9c81262bd92565c1c1af8f99ce3f8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11d6faef622425fe89ee67901364e5d",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "07ba4862d507733bce00",
+    "url": "/rolodex/static/css/main.f35ca79d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ba4862d507733bce00",
+    "url": "/rolodex/static/js/main.1e2f3418.chunk.js"
+  },
+  {
+    "revision": "5b645e36ede34f934ded",
+    "url": "/rolodex/static/js/runtime-main.743d6061.js"
+  }
+]);
