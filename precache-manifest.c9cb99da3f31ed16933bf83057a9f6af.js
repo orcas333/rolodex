dbd63081eab026cb69a96e6ceb1ecfbabf026448
Jenny Hu <huj@mit.edu>
2020-04-27 13:15:08 -0700

    Updates

---
+++ precache-manifest.c9cb99da3f31ed16933bf83057a9f6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26782382576ae5cb67b652a0d9a9df1",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "3c2fbb7fcf6dd3dba1ea",
+    "url": "/rolodex/static/css/main.f35ca79d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2fbb7fcf6dd3dba1ea",
+    "url": "/rolodex/static/js/main.5c597e69.chunk.js"
+  },
+  {
+    "revision": "5b645e36ede34f934ded",
+    "url": "/rolodex/static/js/runtime-main.743d6061.js"
+  }
+]);
